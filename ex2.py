3e9e219cdadfa73e89f0507be4ebf2ba5edda93d
Maxime Eng <maxime.eng@epitech.eu>
2023-01-24 15:06:01 +0100

    [add] ex2

---
+++ ex2.py
@@ -0,0 +1,10 @@
+def calculate(number):
+    if (len(number) == 1) :
+        return(False)
+    else :
+        return (sum(type(int(number)) + 1))
+    # for x in range(number) :
+    #     print(x)
+
+# number = ['1', '2']
+# print(calculate(number))
