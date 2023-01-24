982890d382c676c4297bdb98cc9c911dac521f13
Maxime Eng <maxime.eng@epitech.eu>
2023-01-24 14:21:49 +0100

    [add] ex1

--- ex1.py
+++ ex1.py
@@ -1,4 +1,8 @@
-for num in range(1, 1001) :
+for num in range(1, 100) :
     print(num)
-    if (num % 3 == 0) :
+    if (num % 3 == 0 and num % 5 == 0) :
+        print("ThreeFive")
+    elif (num % 3 == 0) :
         print("Three")
+    elif (num % 5 == 0) :
+        print("Five")
